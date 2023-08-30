516e4db0f3fac0ee55b6121efbaa3a81efd33659
saheb mohammadi <saheb.put92@gmail.com>
2023-08-30 12:01:53 +0430

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import "./App.css";
-import "react-date-range/dist/styles.css"; // main css file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import Header from "./components/Header/Header";
-import { Route, Routes } from "react-router-dom";
-import Room from "./components/Room/Room";
-import Hotels from "./components/Hotels/Hotels";
-import Home from "./components/Home/Home";
-import { Toaster } from "react-hot-toast";
-import AppLayout from "./components/AppLayout/AppLayout";
-import HotelsProvider from "./context/HotelsProvider";
-import Bookmark from "./components/Bookmark/Bookmark";
 
 function App() {
-  return (
-    <HotelsProvider>
-      <Toaster />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<AppLayout />}>
-          <Route index element={<Hotels />} />
-          <Route path=":id" element={<Room />} />
-          <Route path="bookmark" element={<Bookmark />} />
-        </Route>
-      </Routes>
-    </HotelsProvider>
-  );
+  return <h1>09 Starter file</h1>;
 }
 
 export default App;
-
-// hotels
-// hotels/:id
--- src/components/Bookmark/Bookmark.jsx
+++
@@ -1,9 +0,0 @@
-import useUrlPositioin from "../../hooks/useUrlPosition";
-
-function Bookmark() {
-  const [lat, lng] = useUrlPositioin();
-  
-  return <div>Bookmark</div>;
-}
-
-export default Bookmark;
